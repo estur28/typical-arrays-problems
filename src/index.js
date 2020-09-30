2f1062dc5c5cc3de0b7d5c5cf286eaa7efa53ef8
Natalia <safary-1994@mail.ru>
2020-09-30 18:32:23 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    return !array || !array.length ? 0 : Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !array || !array.length ? 0 : Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    const sum = array.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+    return sum / array.length;
+  };
+};
+    
